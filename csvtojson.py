38223a7fb81048fdb536754cbcec493f88a1042f
yanjiunliu <ggyy831214@gmail.com>
2023-07-04 14:31:33 +0800

    Refactor updarte csvtojson.py

--- csvtojson.py
+++ csvtojson.py
@@ -21,7 +21,7 @@ def csv_to_json(csv_file_path: str):
 
 def load_json(json_file_path, data):
     with open(json_file_path, 'a') as jsonfile:
-        jsonfile.write(json.dumps(data))
+        jsonfile.write(f"\t\t{json.dumps(data)}")
 
 
 def main():
@@ -35,23 +35,20 @@ def main():
     for csv_file_path in csv_files:
         key = os.path.basename(csv_file_path).replace("hospital_atg_", "").replace(".csv", "")
         print(f"csv to json: {key} 讀取資料")
-        json_data = {
-            key: []
-        }
         json_file_path = f"json/{key}.json"
 
-        # with open(json_file_path, 'a') as jsonfile:
-        #     jsonfile.write("{\n")
-        #     jsonfile.write(f"\t\"{key}\":[\n")
+        with open(json_file_path, 'a') as jsonfile:
+            jsonfile.write("{\n")
+            jsonfile.write(f"\t\"{key}\":[\n")
         tranform = csv_to_json(csv_file_path)
         value = next(tranform, None)
         while value:
-            json_data[key].append(value)
+            load_json(json_file_path, value)
             value = next(tranform, None)
-        load_json(json_file_path, json_data)
-        # with open(json_file_path, 'a') as jsonfile:
-        #     jsonfile.write("\n]")
-        #     jsonfile.write("\n}")
+
+        with open(json_file_path, 'a') as jsonfile:
+            jsonfile.write("\t\n]")
+            jsonfile.write("\n}")
 
         print(f"csv to json: {key} 轉換完成")
 
